c17161420f69e178258596fc4b674b4ae0dc166c
dev-madhurendra <madhurendraakela123@gmail.com>
2023-11-06 17:34:18 +0530

    feat : added array syntax in golang

---
+++ 08array/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func cStoI(elementStr string) (int64, error) {
+	return strconv.ParseInt(strings.TrimSpace(elementStr), 10, 64)
+}
+
+func main() {
+
+	// fixed length
+	var fruits [3]string
+	fruits[0] = "Mangoes"
+	fruits[1] = "Apple"
+	fruits[2] = "Banana"
+
+	fmt.Println("List of fruits : ", fruits)
+
+	// Create a bufio.Reader to read from standard input (os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
+
+	// Ask the user to specify the length of the slice
+	fmt.Print("Enter the length of the slice: ")
+	lengthStr, _ := reader.ReadString('\n')
+
+	length, err := cStoI(lengthStr)
+
+	// Create a slice to store the user's input values
+	numbers := make([]int64, length)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Read and store user input data
+	for i := int64(0); i < length; i++ {
+		fmt.Printf("Enter element %d: ", i)
+		elementStr, _ := reader.ReadString('\n')
+		element, err := cStoI(elementStr)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		numbers[i] = element
+	}
+
+	// Print the resulting slice
+	fmt.Println("Your slice:", numbers)
+}
