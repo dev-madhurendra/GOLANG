e38608815d4b18eaa9babd6495206ec94f37a887
dev-madhurendra <madhurendraakela123@gmail.com>
2023-11-07 15:44:28 +0530

    feat : added label,break,continue thing in loop

--- 13loop/main.go
+++ 13loop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satruday"}
+	days := []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satruday", "Sunday"}
 
 	fmt.Println(days)
 
@@ -12,7 +12,29 @@ func main() {
 	}
 
 	for i := range days {
-		fmt.Println(days[i])
+		if days[i] == "Saturday" || days[i] == "Sunday" {
+			fmt.Println("Weekdays")
+			continue
+		} else {
+			fmt.Println("Working day : ", days[i])
+		}
 	}
 
+	rogueValue := 1
+
+	for rogueValue < 10 {
+
+		if rogueValue == 7 {
+			rogueValue++
+			goto goto_statement
+			// continue
+		} else if rogueValue == 9 {
+			break
+		} else {
+			fmt.Println("Value is : ", rogueValue)
+		}
+		rogueValue++
+	}
+goto_statement:
+	fmt.Println("Jumping to the line number 38")
 }
